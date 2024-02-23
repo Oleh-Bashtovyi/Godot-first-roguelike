7d8910b56f6020eb38fae093b8b8c2c6a086b872
Oleh-Bashtovyi <olehbashtovyi@gmail.com>
2024-02-24 00:16:39 +0200

    Level generation fully implemented.

--- game.cs
+++ game.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public partial class game : Node2D
 {
@@ -35,6 +36,7 @@ public partial class game : Node2D
 	//CURRENT LEVEL
 	//==============================================================
 	public int current_level_number = 0;
+	public Vector2 LevelSize = new Vector2();
 	public Vector2 RoomSize = new Vector2();
 	public List<Rect2> Rooms = new();
 	public List<List<TileType>> Map = new();
@@ -55,7 +57,8 @@ public partial class game : Node2D
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
 	{
-		DisplayServer.WindowSetSize(new Vector2I(1280, 720));
+		//DisplayServer.WindowSetSize(new Vector2I(1280, 720));
+		DisplayServer.WindowSetSize(new Vector2I(1920, 1080));
 		_player = this.GetNode<Sprite2D>(new NodePath("Player"));
 		_TileMap = this.GetNode<TileMap>(new NodePath("TileMap"));
 		BuildLevel();
@@ -74,12 +77,12 @@ public partial class game : Node2D
 		Map.Clear();
 		_TileMap.Clear();
 
-		var levelSize = LEVEL_SIZES[current_level_number];
+		LevelSize = LEVEL_SIZES[current_level_number];
 
-		for (int x = 0; x < levelSize.X; x++)
+		for (int x = 0; x < LevelSize.X; x++)
 		{
 			Map.Add(new());
-			for (int y = 0; y < levelSize.Y; y++)
+			for (int y = 0; y < LevelSize.Y; y++)
 			{
 				//SetTile(x, y, TileType.Stone);
 				Map[x].Add(TileType.Stone);
@@ -87,7 +90,7 @@ public partial class game : Node2D
 			}
 		}
 
-		var freeRegions = new List<Rect2>() { new Rect2(new Vector2(2, 2), levelSize - new Vector2(4, 4)) };
+		var freeRegions = new List<Rect2>() { new Rect2(new Vector2(2, 2), LevelSize - new Vector2(4, 4)) };
 		var numberOfRoom = LEVEL_ROOMS_COUNT[current_level_number];
 
 		for (int i = 0; i < numberOfRoom; i++)
@@ -99,11 +102,179 @@ public partial class game : Node2D
 				break;
 			}
 		}
+		ConnectRooms();
+	}
 
+	private void ConnectRooms()
+	{
+		var stoneGraph = new AStar2D();
+		var pointId = 0l;
+
+		//build AStart graph for stones tiles
+		for (int x = 0; x < LevelSize.X; x++)
+		{
+			for (int y = 0; y < LevelSize.Y; y++)
+			{
+				if (Map[x][y] == TileType.Stone)
+				{
+					var weight = 0;
+					stoneGraph.AddPoint(pointId, new Vector2I(x, y), weight);
+
+					//connect to left if also stone
+					if(x > 0 && Map[x-1][y] == TileType.Stone)
+					{
+						var leftPoint = stoneGraph.GetClosestPoint(new Vector2I(x - 1, y));
+						stoneGraph.ConnectPoints(pointId, leftPoint);
+					}
+
+					//connect to above if also stone
+					if(y > 0 && Map[x][y - 1] == TileType.Stone)
+					{
+						var abovePoint = stoneGraph.GetClosestPoint(new Vector2I(x, y - 1));
+                        stoneGraph.ConnectPoints(pointId, abovePoint);
+                    }
+					pointId++;
+				}
+			}
+		}
+
+		//Build an AStar graph for rooms connection
+		var roomGraph = new AStar2D();
+		pointId = 0;
+		foreach(var room in Rooms)
+		{
+			roomGraph.AddPoint(pointId, room.GetCenter(), 0);
+			pointId++;
+		}
+
+		//add random connection untill everything is connected
+		while (!IsEverythingConnected(roomGraph))
+		{
+			AddRandomConnection(stoneGraph, roomGraph);
+		}
 	}
 
 
-	private void AddRoom(List<Rect2> freeRegions)
+	private bool IsEverythingConnected(AStar2D roomGraph)  
+	{
+		var point =  roomGraph.GetPointIds().First();
+		return roomGraph.GetPointConnections(point).Count() == roomGraph.GetPointCount() - 1;
+	}
+
+	private void AddRandomConnection(AStar2D stoneGraph, AStar2D roomGraph)
+	{
+		var startRoomId = GetLeastConnectedPoint(roomGraph);
+		var endRoomId = GetNearestUnconectedPoint(roomGraph, startRoomId);
+
+		//pick door location
+		var startPosition = PickRandomDoorLocation(Rooms[(int)startRoomId]);
+		var endPosition = PickRandomDoorLocation(Rooms[(int)endRoomId]);
+
+		//find path to connect doors to each others
+		var closestStartPoint = stoneGraph.GetClosestPoint(startPosition);
+		var closestEndPoint = stoneGraph.GetClosestPoint(endPosition);
+
+		var path = stoneGraph.GetPointPath(closestStartPoint, closestEndPoint);
+		
+		SetTile((int)startPosition.X, (int)startPosition.Y, TileType.Door);
+		SetTile((int)endPosition.X, (int)endPosition.Y, TileType.Door);
+
+		foreach(var position in path)
+		{
+			SetTile((int)position.X, (int)position.Y, TileType.Floor);
+		}
+
+		roomGraph.ConnectPoints(startRoomId, endRoomId);
+	}
+
+
+	private long GetLeastConnectedPoint(AStar2D roomGraph)
+	{
+		var pointsIds = roomGraph.GetPointIds();
+		var tiedForLeast = new List<long>();
+		var least = 1_000_000l;
+
+		foreach(var pointId in pointsIds)
+		{
+			var count = roomGraph.GetPointConnections(pointId).Count();
+
+			if(count < least)
+			{
+				least = count;
+				tiedForLeast = new List<long>() { pointId };
+            }
+			else if(count == least)
+			{
+				tiedForLeast.Add(pointId);
+			}
+		}
+
+		var rand = new Random();
+		return tiedForLeast[rand.Next() % tiedForLeast.Count];
+	}
+
+	private long GetNearestUnconectedPoint(AStar2D roomGraph, long targetPoint)
+	{
+		var targetPosition = roomGraph.GetPointPosition(targetPoint);
+		var pointsIds = roomGraph.GetPointIds();
+
+		var nearest = 1_000_000l;
+		var tiedForNearest = new List<long>();
+
+		foreach(var pointId in pointsIds)
+		{
+			if(pointId == targetPoint)
+			{
+				continue;
+			}
+
+			var path = roomGraph.GetPointPath(pointId, targetPoint);
+			if(path != null && path.Length != 0)
+			{
+				continue;
+			}
+
+			var dist = (roomGraph.GetPointPosition(pointId) - targetPosition).Length();
+			if(dist < nearest)
+			{
+				nearest = (long)dist;
+				tiedForNearest = new List<long>() { pointId };
+			}
+			else if ((long)dist == nearest)
+			{
+				tiedForNearest.Add(pointId);
+			}
+		}
+        var rand = new Random();
+        return tiedForNearest[rand.Next() % tiedForNearest.Count];
+    }
+
+
+	private Vector2 PickRandomDoorLocation(Rect2 room)
+	{
+		var options = new List<Vector2>();
+
+		//Top and bottom walls
+		for (int x = (int)room.Position.X + 1; x < room.End.X - 2; x++)
+		{
+			options.Add(new (x, room.Position.Y));
+			options.Add(new (x, room.End.Y - 1));
+		}
+
+		//left and right walls
+		for (int y = (int)room.Position.Y + 1; y < room.End.Y - 2; y++)
+		{
+			options.Add(new (room.Position.X, y));
+			options.Add(new (room.End.X - 1, y));
+		}
+
+		var rand = new Random();
+		return options[rand.Next() % options.Count];
+	}
+
+
+
+    private void AddRoom(List<Rect2> freeRegions)
 	{
 		var rand = new Random();
 		var region = freeRegions[rand.Next() % freeRegions.Count];
@@ -157,8 +328,6 @@ public partial class game : Node2D
 
 		CutRegions(freeRegions, room);
     }
-
-
 
 	private void CutRegions(List<Rect2> freeRegions, Rect2 regionToRemove)
 	{
